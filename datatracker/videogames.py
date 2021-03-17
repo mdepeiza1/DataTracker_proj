5e6d4c506e827b4d34170736f5393c5f3c8f24d7
Micah de Peiza <mdepeiza@keyconceptskb.com>
2021-03-17 11:39:04 -0400

    Investment chart implemented.

--- datatracker/videogames.py
+++ datatracker/videogames.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, redirect, flash, render_template, url_for, Blueprint
 from .models.videogames import VideoGames
 from .models.videogames import videogames
+import html
 
 bp = Blueprint('videogames', __name__)
 
@@ -11,6 +12,12 @@ def Investment():
     xlabels = []
     ylabels = []
     for videogame in videogames:
-        xlabels.append(videogame.platform)
-        ylabels.append(videogame.globalSales)
-    return render_template('videogames/Investment.html', xs=xlabels, ys=ylabels)
+        if videogame.platform in xlabels:
+            indexOfPlatform = xlabels.index(videogame.platform)
+            ylabels[indexOfPlatform] += videogame.globalSales
+        else:
+            xlabels.append(videogame.platform)
+            ylabels.append(videogame.globalSales)
+    xs = html.unescape(xlabels)
+    ys = html.unescape(ylabels)
+    return render_template('videogames/Investment.html', xs=xs, ys=ys)
