4e58e5eb6ef7bff6a8a670c10e57f6c1f9f2dc5c
deisaacs98 <deisaacs98@gmail.com>
2021-03-17 13:37:07 -0500

    Added game details to search results page.

--- datatracker/videogames.py
+++ datatracker/videogames.py
@@ -42,6 +42,8 @@ def PublisherPerConsole():
     ys = html.unescape(ylabels)
     return render_template('videogames/PublisherPerConsole.html', xs=xs, ys=ys)
 
+
+
 @bp.route('/Search',methods=['GET','POST'])
 def SearchName():
     if request.method == 'POST':
@@ -62,8 +64,11 @@ def SearchName():
                     else:
                         xlabels.append(videogame.platform)
                         ylabels.append(videogame.globalSales)
-                xs = html.unescape(xlabels)
-                ys = html.unescape(ylabels)
-                return render_template('videogames/SearchResults.html', page_title=game_title, xs=xs, ys=ys)
+                    xs = html.unescape(xlabels)
+                    ys = html.unescape(ylabels)
+                    return render_template('videogames/SearchResults.html', page_title=game_title, xs=xs, ys=ys,
+                                           video_game=videogame)
+
+
     else:
         return render_template('videogames/Search.html')
